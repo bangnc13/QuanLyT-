3fd42a9cfc2440d3e77ad987d0ef2f54f56dbb51
bangnc13 <bangnc13@fpt.com>
2026-09-06 11:31:52 +0700

    Update App.py

--- App.py
+++ App.py
@@ -98,7 +98,7 @@ def load_excel_data():
 
 df, file_name = load_excel_data() 
 
-st.sidebar.markdown('<div class="sidebar-title">🗺️ TỐI ƯU LỘ TRÌNH TẬP ĐIỂM</div>', unsafe_allow_html=True)
+st.sidebar.markdown('<div class="sidebar-title"></div>', unsafe_allow_html=True)
 st.sidebar.markdown('<div class="sidebar-subtitle">Tối ưu quãng đường thu cước - Make by BangNC13 </div>', unsafe_allow_html=True)
 
 # Khởi tạo session state kích hoạt tối ưu từ sidebar
